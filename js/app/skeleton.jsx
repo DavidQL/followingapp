e16dd53a03deff181aaf8866ab3f064f6c06b9a0
David Aragon <david@quickleft.com>
2014-08-08 21:01:37 -0600

    stream parser

--- js/app/skeleton.jsx
+++ js/app/skeleton.jsx
@@ -1,5 +1,6 @@
 var React = require('./../../bower_components/react/react-with-addons');
 var $ = require('./../../bower_components/jquery/dist/jquery');
+var streamParser = require('./stream_parser');
 var Thermometer = require('./thermometer.jsx');
 var Question = require('./question.jsx');
 
---
+++ js/app/stream_parser.js
@@ -0,0 +1,91 @@
+
+
+
+return {
+  round1: [
+    {
+      tweet: {
+        body: 
+        date:
+      },
+      people: [
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        }
+      ]
+    },
+    {
+      tweet: {
+        body: 
+        date:
+      },
+      people: [
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        }
+      ]
+    },
+    {
+      tweet: {
+        body: 
+        date:
+      },
+      people: [
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        },
+        {
+          photoUrl: 
+          handle:
+          isAuthor:
+        }
+      ]
+    }
+  ]
+}
