ef7653f4c7bd3d0e6eeb528332518a572ba353a5
David Aragon <david@quickleft.com>
2014-08-10 20:00:09 -0600

    styling

--- js/app/question/round1.jsx
+++ js/app/question/round1.jsx
@@ -1,5 +1,6 @@
 var React = require('./../../../bower_components/react/react-with-addons');
 var _ = require('underscore');
+var moment = require('moment');
 var AvatarGallery = require('./components/avatar_gallery.jsx');
 
 module.exports = React.createClass({
@@ -10,11 +11,15 @@ module.exports = React.createClass({
     },
 
     render: function() {
+      var formattedDate = moment(this.props.game_data[0].tweet.date).format('h:mm a - D MMM YYYY');
       return (
-        <div className="col-md-6">
+        <div className="col-md-6 question">
           <div className="row">
             <div className="col-md-9 tweet center-block">
               {this.props.game_data[0].tweet.body}
+              <div className="date">
+                {formattedDate}
+              </div>
             </div>
           </div>
 
