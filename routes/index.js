46b2c730498ececdabfd87430d118b2c0425f9d1
David Aragon <david@quickleft.com>
2014-08-21 22:14:40 -0600

    styling / iOS viewport / logout

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,19 @@ module.exports = function(app) {
   var express = require('express');
   var router = express.Router();
 
-  return router.get('/', function(req, res) {
+  router.get('/', function(req, res) {
     if (req.session.profile) {
       res.redirect('/game');
     } else {
       res.render('index', { title: 'Following'});
     }
   });
+
+  router.get('/logout', function(req, res) {
+    req.session.destroy(function(err) {
+      res.redirect('/');
+    });
+  });
+
+  return router;
 };
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,11 +1,14 @@
 module.exports = function(app) {
   var express = require('express');
   var router = express.Router();
-  var sample_tweets = require('../sample-tweets');
+  var sample_tweets = false; require('../sample-tweets');
 
   return router.get('/', function(req, res) {
     if (sample_tweets) {
       res.send(sample_tweets);
+    }
+    else if (req.session.tweets) {
+      res.send(req.session.tweets);
     } else {
       app.consumer.get("https://api.twitter.com/1.1/statuses/home_timeline.json?count=200", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
         req.session.tweets = JSON.parse(data);
