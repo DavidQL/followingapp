a6b4792e40c66bead6d4a9e8f22c257c967bfe89
David Aragon <david@quickleft.com>
2014-08-23 11:39:29 -0600

    fix twitter consumer secret bug

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var keys = (function() {
     if (process.env._twitterConsumerKey) {
         return {
             "_twitterConsumerKey": process.env._twitterConsumerKey,
-            "_twitterConsumerSecret": process.env._twitterConsumerKey,
+            "_twitterConsumerSecret": process.env._twitterConsumerSecret,
             "session_token": process.env.session_token,
             "env": "production"
         }
